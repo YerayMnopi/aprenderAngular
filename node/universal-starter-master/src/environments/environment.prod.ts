d7c1420f0e0e53ed7c34c63ba9af0e23926768c0
YerayMnopi <yeraysonido@yahoo.es>
2018-04-22 00:40:04 +0200

    fix(docker-compose): Set nginx default alias to proxy

--- node/universal-starter-master/src/environments/environment.prod.ts
+++ node/universal-starter-master/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'http://www.aprenderangular.com/api/',
-  serverSideApiUrl: 'http://www.aprenderangular.com/api/'
+  serverSideApiUrl: 'http://proxy/api/'
 };
--- node/universal-starter-master/src/environments/environment.ts
+++ node/universal-starter-master/src/environments/environment.ts
@@ -6,5 +6,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000/api/',
-  serverSideApiUrl: 'proxy/api/'
+  serverSideApiUrl: 'http://proxy/api/'
 };
