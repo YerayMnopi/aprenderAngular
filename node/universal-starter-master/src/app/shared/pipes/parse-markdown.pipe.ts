1f3c79badff674cc6497e8d445dc08ec3c60fe27
YerayMnopi <yeraysonido@yahoo.es>
2018-07-13 18:50:25 +0200

    feature(edit-post): Create indent code

--- node/universal-starter-master/src/app/shared/pipes/parse-markdown.pipe.ts
+++ node/universal-starter-master/src/app/shared/pipes/parse-markdown.pipe.ts
@@ -15,6 +15,13 @@ export class ParseMarkdownPipe implements PipeTransform {
       markdown: '**',
       html: 'strong'
     }
+  ];
+
+  codes = [    
+    {
+      markdown: '~>',
+      html: '&nbsp;&nbsp;'
+    }
   ]
 
   transform(paragraph: string): SafeHtml {
@@ -23,20 +30,36 @@ export class ParseMarkdownPipe implements PipeTransform {
       (symbol) => {
         let splittedParagraph = paragraph.split(symbol.markdown);
 
-        paragraph = splittedParagraph.reduce(
-          (previousText: string, currentText: string, currentIndex: number) => {
-            let tag = '<';
-
-            if (currentIndex % 2 !== 0) {
-              tag += '/'; 
-            }
-
-            return previousText + currentText + tag + symbol.html + '>';
-          }, ''
-        );
-
+        if (splittedParagraph.length > 1) {
+          paragraph = splittedParagraph.reduce(
+            (previousText: string, currentText: string, currentIndex: number) => {
+              let tag = '<';
+  
+              if (currentIndex % 2 !== 0) {
+                tag += '/'; 
+              }
+  
+              return previousText + currentText + tag + symbol.html + '>';
+            }, ''
+          );
+        }
       }
     );
+
+    this.codes.forEach(
+      (symbol) => {
+        let splittedParagraph = paragraph.split(symbol.markdown);
+
+        if (splittedParagraph.length > 1) {
+          paragraph = splittedParagraph.reduce(
+            (previousText: string, currentText: string, currentIndex: number) => {  
+              return previousText + currentText + symbol.html;
+            }, ''
+          );
+        }
+      }
+    );
+
     return paragraph;
   }
 }
