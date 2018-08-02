618331bf8aaaf9f60bc7b10817529be7d7b99b47
YerayMnopi <yeraysonido@yahoo.es>
2018-08-02 22:14:22 +0200

    fix(nginx, node): Correct media url folder again

--- node/universal-starter-master/src/environments/environment.prod.ts
+++ node/universal-starter-master/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'http://www.aprenderangular.com/api/',
-  mediaUrl: 'http://media.aprenderangular.com/api/',
+  mediaUrl: 'http://media.aprenderangular.com',
   serverSideApiUrl: 'http://www.aprenderangular.com/api/',
   newElementCode: 'nuevoElemento'
 };
